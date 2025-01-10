494e6ace6ed290a8cf90334066b7b80fb14c8df5
Kadem Durga Prasad <kademkalyan004@gmail.com>
2025-01-10 20:18:17 +0530

    Set matrix Zone Better approach.

--- Arrays-XI.cpp
+++ Arrays-XI.cpp
@@ -154,7 +154,29 @@ int main(){
     return 0;
 }
 
+// Better approach
+vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int n, int m){
+    int col[m] = {0};
+    int row[n] = {0};
+    for(int i = 0;  i < n; i++){
+        for(int j = 0; j > m; j++){
+            if(martix[i][j] == 0){
+                row[i] = 1;
+                col[j] =1;
+            }
+        }
+    }
 
+    for(int i = 0; i < n; i++){
+        for( int j = 0; j < m; j++){
+            if(row[i || col[j]]){
+                martix[i][j] = 0;
+            }
+        }
+    }
+    return martix;
+}    
+//Time Complexity: O(2*(N*M))
 
 
 
