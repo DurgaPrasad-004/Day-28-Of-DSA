cdd1b78482a8a7768f5dc141c6ffc4ac367e2666
Kadem Durga Prasad <kademkalyan004@gmail.com>
2025-01-10 20:19:02 +0530

    Set matrix Zone Optimal approach.

--- Arrays-XI.cpp
+++ Arrays-XI.cpp
@@ -178,7 +178,47 @@ vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int n, int m){
 }    
 //Time Complexity: O(2*(N*M))
 
+// Optimal Approach
+vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int n, int m){
+    int col0 = 1; 
+    // int col[m] = {0}; -> martix[0][j]
+    // int row[n] = {0}; -> matrix[0][i]
 
+    // step 1: Traverse the matrix and
+    // mark 1st row & col accordingly:
+     for(int i = 0;  i < n; i++){
+        for(int j = 0; j > m; j++){
+            if(martix[i][j] == 0){
+               martix[i][0] = 0;
+               if( j != 0){
+                martix[0][j] = 0;
+               } else {
+                col0 = 0;
+               }
+            }
+        }
+    }
+
+    // Step 2: Mark with 0 from (1,1) to (n-1, m-1):
+    for(int i = 1; i < n; i++){
+        for(int j = 1; j < m; j++){
+            if(martix[i][j] != 0){
+                if(martix[0][j] == 0 || martix[i][0] == 0){
+                    martix[i][j] = 0;
+                }
+            }
+        }
+    }
+    //step 3: Finally mark the 1st col & then 1st row:
+    if(martix[0][0] == 0){
+        for(int j = 0; j < m; j++) martix[0][j] = 0;
+    }
+    if(col0 == 0){
+        for(int i = 0; i < n; i++){
+            martix[i][0] = 0;
+        }
+    }
+}
 
 
 
